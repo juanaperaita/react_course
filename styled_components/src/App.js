5beaed813b7f598677789619ded03fdeafd560a3
juanaperaita <juanaperai26@gmail.com>
2025-01-07 09:46:35 +0100

    ✨ Add styled_components app

---
+++ styled_components/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { MiBoton } from './styled';
+
+function App() {
+  return (
+    <div className="App">
+      <MiBoton>Log in</MiBoton>
+    </div>
+  );
+}
+
+export default App;
